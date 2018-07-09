fd34abb8f9f1c855057afda25eaa1a6cc8ea4988
John Kruse <johnalphonsekruse@gmail.com>
2018-07-09 18:00:19 -0400

    adding query options for parser

--- osm_parser.py
+++ osm_parser.py
@@ -8,4 +8,14 @@ def query(query_line):
     return soup
 
 def build_query(**kwargs):
-    query = "node({south}, {west}, {north}, {east});->.box;".format(kwargs)
+    #building our box here
+    query = "node({south}, {west}, {north}, {east})->.box;".format(kwargs)
+    if(kwargs["tourism"]):
+        query = query + "(node.box[tourism];)->.results;"
+    if(kwargs["food"]):
+        query = query + "(node.box[amenity=restaurant]; node.box[amenity=cafe]; node.box[amenity=biergarten]; node.box[amenity=bar]; node.box[amenity=pub];)->.restaurants;"
+    if(kwargs["transit"]):
+        query = query + "(node.box[public_transport];node.box[amenity=bicycle_rental];node.box[amenity=bus_station];node.box[amenity=car_rental];node.box[amenity=ferry_terminal];node.box[amenity=taxi];)->.transit;"
+    if(kwargs["nightlife"]):
+        query = query + "(node.box[amenity=nightclub];node.box[amenity=bar] - node.restaurants)->.nightlife;"
+    query = query + "(node.results, node.restaurants, node.transit, node.nightlift);out;)"
