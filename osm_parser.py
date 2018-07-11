15582fecedbe6a1b0ecf15c2cceec9670184d913
John Kruse <johnalphonsekruse@gmail.com>
2018-07-11 14:55:23 -0400

    fixing some querying syntax for overpass

--- osm_parser.py
+++ osm_parser.py
@@ -8,15 +8,14 @@ def send_query(query_line):
 
 def build_query(**kwargs):
     #building our box here
-    query = "[out:json];(node({south[0]}, {west[0]}, {north[0]}, {east[0]}))->.box;".format(**kwargs)
-    print(query)
+    query = "[out:json];(node({south[0]}, {west[0]}, {north[0]}, {east[0]});)->.box;".format(**kwargs)
     if "tourism" in kwargs:
         query = query + "(node.box[tourism];)->.tourism;"
     if "food" in kwargs:
-        query = query + "(node.box[amenity=restaurant]; node.box[amenity=cafe]; node.box[amenity=biergarten]; node.box[amenity=bar]; node.box[amenity=pub];)->.restaurants;"
+        query = query + "(node.box[amenity=restaurant];node.box[amenity=cafe];node.box[amenity=biergarten];node.box[amenity=bar];node.box[amenity=pub];)->.restaurants;"
     if "transit" in kwargs:
         query = query + "(node.box[public_transport];node.box[amenity=bicycle_rental];node.box[amenity=bus_station];node.box[amenity=car_rental];node.box[amenity=ferry_terminal];node.box[amenity=taxi];)->.transit;"
     if "nightlife" in kwargs:
-        query = query + "(node.box[amenity=nightclub];node.box[amenity=bar] - node.restaurants)->.nightlife;"
-    query = query + "(node.tourism, node.restaurants, node.transit, node.nightlife);out;)"
+        query = query + "(node.box[amenity=nightclub];node.box[amenity=bar]; - node.restaurants;)->.nightlife;"
+    query = query + "(node.tourism;node.restaurants;node.transit;node.nightlife;);out;"
     return send_query(query)
