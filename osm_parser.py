b29edce7c6c9216dffb8a421c084b1ae6702f2fa
John Kruse <johnalphonsekruse@gmail.com>
2018-07-11 12:36:29 -0400

    fixing some code, changing names, getting return as json

--- osm_parser.py
+++ osm_parser.py
@@ -1,21 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-def query(query_line):
+def send_query(query_line):
     payload = {'data': query_line}
     result = requests.get('https://lz4.overpass-api.de/api/interpreter', params=payload)
-    soup = BeautifulSoup(result.content, "xml")
-    return soup
+    return result
 
 def build_query(**kwargs):
     #building our box here
-    query = "node({south}, {west}, {north}, {east})->.box;".format(kwargs)
-    if(kwargs["tourism"]):
-        query = query + "(node.box[tourism];)->.results;"
-    if(kwargs["food"]):
+    query = "[out:json];(node({south}, {west}, {north}, {east}))->.box;".format(kwargs)
+    if "tourism" in kwargs:
+        query = query + "(node.box[tourism];)->.tourism;"
+    if "food" in kwargs:
         query = query + "(node.box[amenity=restaurant]; node.box[amenity=cafe]; node.box[amenity=biergarten]; node.box[amenity=bar]; node.box[amenity=pub];)->.restaurants;"
-    if(kwargs["transit"]):
+    if "transit" in kwargs:
         query = query + "(node.box[public_transport];node.box[amenity=bicycle_rental];node.box[amenity=bus_station];node.box[amenity=car_rental];node.box[amenity=ferry_terminal];node.box[amenity=taxi];)->.transit;"
-    if(kwargs["nightlife"]):
+    if "nightlife" in kwargs:
         query = query + "(node.box[amenity=nightclub];node.box[amenity=bar] - node.restaurants)->.nightlife;"
-    query = query + "(node.results, node.restaurants, node.transit, node.nightlift);out;)"
+    query = query + "(node.tourism, node.restaurants, node.transit, node.nightlife);out;)"
+    return send_query(query)
